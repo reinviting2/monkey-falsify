62af9de2e50990aaf03ed727e867f373eeb222f0
reinviting2 <thegreatwarmlol@gmail.com>
2025-03-23 21:47:53 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.config = {
   entryPhrase: "getmein",  // phrase to enter the real site
-  proxyLink: "https://example.com", // proxy link, highly recommended you change it
+  proxyLink: "https://hyperbeam.com", // proxy link, highly recommended you change it
   blanker: "byebye", // opens your proxy link in about blank
   leverofdoom: "no",
   alternativeproxy: "https://youtube.com", // alternative Proxy
